19c39b8002e0abbe903fd17cebe1834e5cae1b20
exagyde <nolann.morence@hotmail.fr>
2025-05-31 22:05:29 +0200

    Fix for Progressive Web App

--- card-keeper/src/app/layout.js
+++ card-keeper/src/app/layout.js
@@ -9,7 +9,14 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
+                <meta rel="manifest" href="/manifest.json" />
+                <meta name="theme-color" content="#060a0e" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
                 <meta name="apple-mobile-web-app-title" content="CardKeeper" />
+                <link rel="apple-touch-icon" href="/icon-192x192.png" />
+                <link rel="icon" href="/favicon.ico" />
             </head>
             <body>
                 {children}
